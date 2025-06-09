b4fc6cdc0595dab25041cb63dc3f0fc965b29cba
Inon Guetta <inon16guetta@gmail.com>
2025-06-09 17:18:10 +0300

    Add project files.

---
+++ Malshinon/classes/Lealshin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace Malshinon.classes
+{
+    internal class Lealshin
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string secret_code {get; set;}
+        public int status { get; set; }
+        public string malshin_about { get; set; }
+
+
+    }
+}
