8c10b3e7d754b08a2a7c9d512c0a6e97398cc4db
Inon Guetta <inon16guetta@gmail.com>
2025-06-11 17:43:35 +0300

    insert workoing

--- Malshinon/DAL/LealshinDAL.cs
+++ Malshinon/DAL/LealshinDAL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using Malshinon.classes;
@@ -16,30 +17,49 @@ namespace Malshinon.DAL
         string connStr = "server=localhost;username=root;password=;database=malshinon;";
         //-------------------------------------------------------------------------------------
         // לבין זה 
-        public void addToLealshin(Lealshin lealshin)
-        {
-            // שים לב כי זה צריך להיות מחוץ ל using
-            string query = "INSERT INTO malshinim(malshin_first_name,malshin_last_name,malshin_secret_code,malshin_status,malshin_about)" +
-                "VALUES (@malshin_first_name,@malshin_last_name,@malshin_secret_code,@malshin_status,@malshin_about)";
+        //public void addToLealshin(Lealshin lealshin)
+        //{
+        //    // שים לב כי זה צריך להיות מחוץ ל using
+        //    string query = "INSERT INTO malshinim(malshin_first_name,malshin_last_name,malshin_secret_code,malshin_status,malshin_about)" +
+        //        "VALUES (@malshin_first_name,@malshin_last_name,@malshin_secret_code,@malshin_status,@malshin_about)";
 
-            using(MySqlConnection conn = new MySqlConnection(connStr)){
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand(query,conn);
-                cmd.Parameters.AddWithValue(@"malshin_first_name", lealshin.malshin_first_name);
-                cmd.Parameters.AddWithValue(@"malshin_last_name", lealshin.malshin_last_name);
-                cmd.Parameters.AddWithValue(@"malshin_secret_code", lealshin.malshin_secret_code);
-                //cmd.Parameters.AddWithValue(@"malshin_status", lealshin.malshin_status);
-                cmd.Parameters.AddWithValue(@"malshin_about", lealshin.malshin_about);
+        //    using(MySqlConnection conn = new MySqlConnection(connStr)){
+        //        conn.Open();
+        //        MySqlCommand cmd = new MySqlCommand(query,conn);
+        //        cmd.Parameters.AddWithValue(@"malshin_first_name", lealshin.malshin_first_name);
+        //        cmd.Parameters.AddWithValue(@"malshin_last_name", lealshin.malshin_last_name);
+        //        cmd.Parameters.AddWithValue(@"malshin_secret_code", lealshin.malshin_secret_code);
+        //        cmd.Parameters.AddWithValue(@"malshin_about", lealshin.malshin_about);
 
+        //        cmd.ExecuteNonQuery();
+        //    }
 
-
-
-                cmd.ExecuteNonQuery();
-            }
-
-        }
+        //}
         //-------------------------------------------------------------------------------------
+        
+        public string InsertHalshanaFromClient(string first_name,string last_name, string secret_code,string malshin_about) 
+        {
+            string query = $"INSERT INTO malshinim (malshin_first_name,malshin_last_name,malshin_secret_code,malshin_about ) " +
+                $"VALUES  ('{first_name}','{last_name}','{secret_code}','{malshin_about}');";
+            return query;
+        } 
 
+        public void PushSql(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"err: {e}");
+                }
+            }
+        }
 
     }
 }
