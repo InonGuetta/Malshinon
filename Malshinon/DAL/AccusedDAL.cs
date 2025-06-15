5d759c2dd5a4f60fb0f3c2f4ee78dd9b99a5605f
Inon Guetta <inon16guetta@gmail.com>
2025-06-15 06:30:42 +0300

    branch_to_finish

--- Malshinon/DAL/AccusedDAL.cs
+++ Malshinon/DAL/AccusedDAL.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.SqlServer.Server;
-using MySql.Data.MySqlClient;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.SqlServer.Server;
+    using MySql.Data.MySqlClient;
 
-namespace Malshinon.DAL
-{
-    internal class AccusedDAL
+    namespace Malshinon.DAL
     {
-        string connStr = "server=localhost;username=root;password=;database=malshinon;";
-
-        //-------------------------------------------------------------------------------------
-        public string InsertHalshanaFromClientToAccused(string molshan)
+        internal class AccusedDAL
         {
-            molshan += " Not_Insert_Last_Name";
-            string[] the_molshan = molshan.Split(' ','-');
+            string connStr = "server=localhost;username=root;password=;database=malshinon;";
 
-            string query = $"INSERT INTO accused (accused_first_name,accused_last_name ) " +
-                $"VALUES  ('{the_molshan[0]}', '{the_molshan[1]}');";
-            return query;
-        }
-        //-------------------------------------------------------------------------------------
-        public void PushSql(string query)
-        {
-            using (MySqlConnection conn = new MySqlConnection(connStr))
+            //-------------------------------------------------------------------------------------
+            public string InsertHalshanaFromClientToAccused(string molshan)
             {
-                try
+                molshan += " Not_Insert_Last_Name";
+                string[] the_molshan = molshan.Split(' ','-');
+
+                string query = $"INSERT INTO accused (accused_first_name,accused_last_name ) " +
+                    $"VALUES  ('{the_molshan[0]}', '{the_molshan[1]}');";
+                return query;
+            }
+            //-------------------------------------------------------------------------------------
+            public void PushSql(string query)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
                 {
-                    conn.Open();
-                    MySqlCommand cmd = new MySqlCommand(query, conn);
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"err: {e}");
+                    try
+                    {
+                        conn.Open();
+                        MySqlCommand cmd = new MySqlCommand(query, conn);
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"err: {e}");
+                    }
                 }
             }
         }
     }
-}
